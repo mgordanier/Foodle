5207ca9ed95fa128aa962702f09eff2b81ccfcbc
Jennifer Yang <jenyang929@gmail.com>
2020-07-17 17:41:46 -0400

    Feat: front page pink css

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,19 +4,41 @@ import {Link} from 'react-router-dom'
 class Home extends Component {
   render() {
     return (
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <img src="/food.jpg" />
-          <img src="/foodlelogo.png" />
+      <div className="columns is-gapless">
+        <div className="column has-background-info">
+          <div className="container">
+            <div className="column is-centered is-vcentered is-mobile">
+              <div className="column is-narrow has-text-centered">
+                <h1 className="title has-text-primary foodle-padding font-family">
+                  POLLING MADE EASY
+                </h1>
+              </div>
+            </div>
+          </div>
 
-          <h2 className="subtitle class-test">
-            Create and plan an event with friends
-          </h2>
-          <Link to="/home">
-            <button type="button" className="button is-primary is-large">
-              Get Started
-            </button>
-          </Link>
+          <div className="container">
+            <div className="column is-centered is-vcentered is-mobile">
+              <div className="column is-narrow has-text-centered">
+                <Link to="/home">
+                  <button type="button" className="button is-primary is-large">
+                    Get Started
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="column is-centered is-vcentered is-mobile">
+            <div className="column is-narrow has-text-centered">
+              <h1 className="has-text-primary text-padding font-family">
+                FOR INDECISIVE FOODLERS
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="column pink-image">
+          <img src="/pinkfoodpartial.png"></img>
         </div>
       </div>
     )
